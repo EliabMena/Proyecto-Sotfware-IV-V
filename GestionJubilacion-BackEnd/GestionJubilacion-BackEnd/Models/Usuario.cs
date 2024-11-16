d3c55da119eb2e2cfb2d84b18d5b7fafa8e45d2c
RamsesSzobotka <vlastoskr@gmail.com>
2024-11-15 21:15:43 -0500

    .

--- GestionJubilacion-BackEnd/GestionJubilacion-BackEnd/Models/Usuario.cs
+++ GestionJubilacion-BackEnd/GestionJubilacion-BackEnd/Models/Usuario.cs
@@ -3,11 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GestionJubilacion_BackEnd.Models
 {
-    public class Usuario
+    public class usuario
     {
         [Key]
         public string cedula { get; set; }
-        public int id_usuario { get; set; }
+        public int id_usuario{ get;set;}
         public string contraseña_hash { get; set; }
         public string nombre { get; set; }
         public string direccion { get; set; }
