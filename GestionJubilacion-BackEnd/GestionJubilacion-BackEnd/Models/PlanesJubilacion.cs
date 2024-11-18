3a8c1d104a7a33297fc32f5a75b6ffebce0e4258
RamsesSzobotka <vlastoskr@gmail.com>
2024-11-18 16:42:11 -0500

    1. controlador para obtener los datos de todos los planes
    2.controlador para obtener una tupla de datos de un plan especifico vinculado aun usuario(para ver el plan activo especificamente para ese usuario)

--- GestionJubilacion-BackEnd/GestionJubilacion-BackEnd/Models/PlanesJubilacion.cs
+++ GestionJubilacion-BackEnd/GestionJubilacion-BackEnd/Models/PlanesJubilacion.cs
@@ -1,10 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestionJubilacion_BackEnd.Models
 {
     public class PlanesJubilacion
     {
         [Key]
-        public int id_plan { get; set; }
+        public int id_plan { get; set; } 
+        public string tipo_plan { get; set; } 
+        public decimal monto_minimo_aportacion { get; set; } 
+        public decimal tasa_interes { get; set; } 
+        public string descripcion { get; set; } 
+        public int cuotas_necesarias { get; set; } 
+
     }
 }
