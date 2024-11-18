50adc4ed3a02f506767acb85364984799759fba2
ArrozConMichi <williamandres507@gmail.com>
2024-11-17 19:38:27 -0500

    Login bien perron

---
+++ GestionJubilacion-FrontEnd/JScripts/scriptreg.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+const btnSesion = document.getElementById("btn-sesion");
+const btnRegistro = document.getElementById("btn-registro");
+
+btnSesion.addEventListener("click", ()=> {
+  container.classList.remove("toggle");
+});
+
+btnRegistro.addEventListener("click", ()=> {
+  container.classList.add("toggle");
+});
+
+
